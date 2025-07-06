ece5b35ba69f1acc97587a48a70446e6e94b62e7
Yashvardhan Goel <yash2002goel@gmail.com>
2025-07-06 14:33:54 +0530

    Add primitive_types.js to define and log primitive data types

---
+++ primitive_types.js
@@ -0,0 +1,13 @@
+// primitive types
+
+let name = 'yashvardhan';
+let age = 20;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(name);
+console.log(age);
+console.log(isApproved);
+console.log(firstName);
+console.log(selectedColor);
