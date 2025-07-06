b3ee4fa69fadd00786e1a6871268e8d2cf192399
Yashvardhan Goel <yash2002goel@gmail.com>
2025-07-06 16:54:33 +0530

    Add arrays to reference_types.js, demonstrating array manipulation and logging of array properties

--- reference_types.js
+++ reference_types.js
@@ -19,3 +19,11 @@ console.log(person);
 // without using let, const, var
 a = 10;
 console.log(a);
+
+
+// arrays
+selectedProducts = ['apple', 'banana', 'cherry'];
+selectedProducts[3] = 'orange';
+console.log(selectedProducts);
+console.log(typeof selectedProducts);
+console.log(selectedProducts.length);
