8be0e40caea173b4ce7d83b39fad3b63860c52be
Yashvardhan Goel <yash2002goel@gmail.com>
2025-07-06 14:35:25 +0530

    Update primitive_types.js to include lastName variable and log its type

--- primitive_types.js
+++ primitive_types.js
@@ -3,11 +3,12 @@
 let name = 'yashvardhan';
 let age = 20;
 let isApproved = false;
-let firstName = undefined;
+let firstName = undefined; let lastName;
 let selectedColor = null;
 
 console.log(name);
 console.log(age);
 console.log(isApproved);
 console.log(firstName);
+console.log(typeof lastName)
 console.log(selectedColor);
