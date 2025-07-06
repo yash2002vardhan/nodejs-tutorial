7e5ce2b9e00b11c1da4738d42cf0cb1697a8c540
Yashvardhan Goel <yash2002goel@gmail.com>
2025-07-06 14:29:33 +0530

    Add variables.js to declare and log variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+// we use let to declare variables
+let a = 10;
+console.log(a);
+
+let b = 'Hello';
+console.log(b);
+
+// Best way to declare variables is to declare them in single separate lines
+let firstName = 'yashvardhan';
+let lastName = 'goel';
+let fullName = firstName + " " + lastName;
+console.log(fullName);
